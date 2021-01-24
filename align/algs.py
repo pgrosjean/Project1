374ef6d8598e8c28875dabfa56fe35f669baa122
Parker Grosjean <parker.grosjean@gmail.com>
2021-01-23 22:09:02 -0800

    updated tests and algorthms.

--- align/algs.py
+++ align/algs.py
@@ -1,24 +1,24 @@
 ##### UPDATES THAT NEED TO HAPPEN #####
-# 1) Modify traceback to allowing branching (ask others)
-# 2) Modify align returned attributes to allow branching
-# 3) Write class doc strings
-# 4) Write up answers to questions in Readme.md
-# 5
+# Implement backtrack table for all of these
+
+import numpy as np
 
 class PairwiseAligner:
 	'''
-	This is the base class for all pairwise aligment algorithms
+	This is the base class for all pairwise alignment algorithms
 	implemented in this API. All pariwise alignment classes
 	inheret from this base class.
 	'''
-	def __init__(self, substitution_matrix_file):
+	def __init__(self, substitution_matrix_file, gap_open, gap_extend):
 		# generating substitution matrix
-		self.sub_matrix = _initiate_sub_matrix(substitution_matrix_file)
+		self.sub_matrix = self._initiate_sub_matrix(substitution_matrix_file)
+		self.gap_open = gap_open
+		self.gap_extend = gap_extend
 		self.seq1 = None
 		self.seq2 = None
 		self.score_matrix = None
 
-	def _initate_sub_matrix(self, matrix_file):
+	def _initiate_sub_matrix(self, matrix_file):
 		'''
 		This function reads in a scoring matrix from any matrix like file.
 		Where there is a line of the residues followed by substitution
@@ -36,29 +36,29 @@ class PairwiseAligner:
 			of the two residues as the key and score as value
 			e.g. {('A', 'A') : 4} or {('A', 'D') : -8}
 		'''
-		with open(file, 'r') as f:
+		with open(matrix_file, 'r') as f:
 			dict_sm = {} # Dictionary for storing scores
 			residue_list = [] # For storing residue list
 			start = False # trigger for reading in score values
 			res_2 = 0 # used for generating substitution matrix
 			# reading file line by line
-			for count, line in enumerate(f):
+			for line_num, line in enumerate(f):
 	            # Reading in Residue List
-				if line.strip().startswith('A ') and start == False:
-					for val in line.strip():
-						if val != ' ':
-							residue_list.append(val)
-							# if residue line encountered then read in matrix
-							start = True
-							# Generating substitution scoring dictionary
-						elif start == True and res_2 < len(residue_list):
-							line = [k for k in line.strip().split(' ') if k != '']
-							line.strip().replace('  ', ' ').split(' ')
-							# reading in line by line to create substitution dictionary
-							for res_1 in range(len(line))[res_2:]:
-								dict_sm[(residue_list[res_1], residue_list[res_2])] = line[res_1]
-								dict_sm[(residue_list[res_2], residue_list[res_1])] = line[res_1]
-								res_2 += 1
+				if '#' not in line.strip() and start == False:
+					residue_list = [k for k in line.strip().upper().split(' ') if k != '']
+					start = True
+				# Generating substitution scoring dictionary
+				elif start == True and res_2 < len(residue_list):
+					line = [k for k in line.strip().split(' ') if k != '']
+					# line.strip().replace('  ', ' ').split(' ')
+					# reading in line by line to create substitution dictionary
+					assert len(residue_list) == len(line)
+					for res_1 in range(len(line)):
+						dict_sm[(residue_list[res_1], residue_list[res_2])] = float(line[res_1])
+						# dict_sm[(residue_list[res_2], residue_list[res_1])] = float(line[res_1])
+					res_2 += 1
+				elif start == True and res_2 == len(residue_list):
+					break
 			return dict_sm
 
 	def _read_fasta(self, fasta_file):
@@ -79,12 +79,12 @@ class PairwiseAligner:
 		with open(fasta_file) as f:
 			is_header = True
 			seq = '' # initializing sequence
-			for line in file:
+			for line in f:
 				if is_header and line.strip().startswith('>'):
 					header = line.strip() # reading in fasta header
 					is_header = False
 				else:
-					seq += line.strip() # generating full seq
+					seq += line.strip().upper() # generating full seq
 		return seq, header
 
 	def _initiate_seqs(self, fasta_1, fasta_2):
@@ -110,7 +110,7 @@ class PairwiseAligner:
 		self.header1 = header1
 		self.header2 = header2
 
-	def _initiate_score_matrix(self):
+	def initiate_matrix(self):
 		'''
 		This function initializes the score matrix of dimensions
 		len(seq1)xlen(seq2). This method must be run after the
@@ -124,8 +124,8 @@ class PairwiseAligner:
 		-------
 		score_matrix (numpy array): array of size len(seq1)xlen(seq2)
 		'''
-		seq1_len = len(self.seq1)
-		seq2_len = len(self.seq2)
+		seq1_len = len(self.seq1) + 1
+		seq2_len = len(self.seq2) + 1
 		score_matrix = np.zeros((seq1_len, seq2_len))
 		return score_matrix
 
@@ -137,11 +137,15 @@ class PairwiseAligner:
 		pass
 
 
+
+################################################################################
+
+
+
+
 class SmithWaterman(PairwiseAligner):
 	def __init__(self, substitution_matrix_file, gap_open, gap_extend):
-		super(SmithWaterman, self).__init__(substitution_matrix_file)
-		self.gap_open = gap_open
-		self.gap_extend = gap_extend
+		super(SmithWaterman, self).__init__(substitution_matrix_file, gap_open, gap_extend)
 
 	def align(self, fasta1, fasta2):
 		'''
@@ -150,7 +154,7 @@ class SmithWaterman(PairwiseAligner):
 		alignmnet. The alignment score is saved to the field
 		self.align_score_ and the alignments are saved as
 		a list of composition [seq1_alignment, seq2_alignment]
-		to the field self.aligment_.
+		to the field self.alignment_.
 
 		Args
 		----
@@ -161,57 +165,97 @@ class SmithWaterman(PairwiseAligner):
 		-------
 		None
 		'''
-		# Initiating Sequences and Scoring Matirx
-		self._initiate_seqs(fasta_1, fasta_2)
-		self.score_matrix = self._initiate_score_matrix()
-		s_matrix = self.score_matrix
-		t_matrix = np.zeros(score_matrix.shape) # seq1 gap matrix
-		u_matrix = np.zeros(score_matrix.shape) # seq2 gap matrix
-		# filling out score matrix (forward pass)
-		for i in range(s_matrix.shape[0])[1:]:
-			for j in range(s_matrix.shape[1])[1:]:
+		# Initializing Sequences
+		self._initiate_seqs(fasta1, fasta2)
+		seq1 = self.seq1
+		seq2 = self.seq2
+		# Initializing Core Score Matrix
+		self.score_matrix = self.initiate_matrix()
+		s_mat = self.score_matrix
+		# Initializing Additional Scoring Matrices for Indels (Gaps)
+		x_mat = self.initiate_matrix() # seq2 gap matrix
+		x_mat[:, 0] = np.arange(len(seq1)+1)*-self.gap_extend - self.gap_open
+		x_mat[0, :] = np.arange(len(seq2)+1)*-self.gap_open*100000000
+		y_mat = self.initiate_matrix() # seq1 gap matrix
+		y_mat[:, 0] = np.arange(len(seq1)+1)*-self.gap_open*100000000
+		y_mat[0, :] = np.arange(len(seq2)+1)*-self.gap_extend - self.gap_open
+		# Initialize Traceback matrix
+		tb_mat = self.initiate_matrix().astype('str')
+		# Filling in first column and row
+		s_mat[1:, 0] = 0
+		s_mat[0, 1:] = 0
+
+		# Filling in score Matrix
+		for i in range(s_mat.shape[0])[1:]:
+			for j in range(s_mat.shape[1])[1:]:
 				# t gap matrix filling
-				t_matrix[i, j] = max([(t_matrix[i-1, j] - self.gap_extend), (s_matrix[i-1, j] - (self.gap_extend + self.gap_open)), 0])
+				gap_dict = {1: 'new_gap', 0: 'extended_gap'}
+				new_gap = (s_mat[i-1, j] - self.gap_open)
+				extended_gap = (x_mat[i-1, j] - self.gap_extend)
+				x_mat[i, j] = max([extended_gap, new_gap])
+				gap_type_x = gap_dict[np.argmax([extended_gap, new_gap])]
 				# u gap matrix filling
-				u_matrix[i, j] = max([(u_matrix[i, j-1] - self.gap_extend), (s_matrix[i, j-1] - (self.gap_extend + self.gap_open)), 0])
-				top = t[i, j]
-				side = u[i, j]
-				diag = s[i-1, j-1] + self.sub_matrix[(seq1[i], seq2[j])]
-				s_matrix[i,j] = max((top, side, diag, 0))
-		# Traceback (backwards pass)
-		max_ind = np.unravel_index(np.argmax(s_matrix, axis=None), s_matrix.shape)
-		self.align_score_ = np.amax(s_matrix)
+				new_gap = (s_mat[i, j-1] - self.gap_open)
+				extended_gap = (y_mat[i, j-1] - self.gap_extend)
+				y_mat[i, j] = max([extended_gap, new_gap])
+				gap_type_y = gap_dict[np.argmax([extended_gap, new_gap])]
+				# Taking maximum transition
+				top = x_mat[i, j]
+				side = y_mat[i, j]
+				diag = s_mat[i-1, j-1] + self.sub_matrix[(seq1[i-1], seq2[j-1])]
+				s_mat[i,j] = max((top, side, diag, 0))
+				max_ind = np.argmax((top, side, diag, 0))
+				# filling in traceback matrix
+				if max_ind == 0 and gap_type_x == 'new_gap':
+					tb_mat[i, j] = 'top_new'
+				elif max_ind == 0 and gap_type_x == 'extended_gap':
+					tb_mat[i, j] = 'top_extended'
+				elif max_ind == 1 and gap_type_y == 'new_gap':
+					tb_mat[i, j] = 'side_new'
+				elif max_ind == 1 and gap_type_y == 'extended_gap':
+					tb_mat[i, j] = 'side_extended'
+				elif max_ind == 2:
+					tb_mat[i, j] = 'diag'
+				elif max_ind == 3:
+					tb_mat[i, j] = 'end'
+		# Traceback
+		alignment_seq1 = ''
+		alignment_seq2 = ''
+		self.align_score_ = s_mat[-1, -1]
+		max_ind = np.unravel_index(np.argmax(s_mat, axis=None), s_mat.shape)
+		self.align_score_ = np.amax(s_mat)
 		i, j = max_ind
-		alignment_seq1 = seq1[i]
-		alignment_seq2 = seq2[j]
 		while i > 0 and j > 0:
-			top = s_matrix[i-1, j]
-			side = s_matrix[i, j-1]
-			diag = s_matrix[i-1, j-1]
-			best_ind = np.argmax(np.array([top, side, diag]))
-			best_max = np.amax(np.array([top, side, diag]))
-			if best_max == 0:
+			tb_val = tb_mat[i, j]
+			if tb_val == 'top_extended':
+				alignment_seq2 = '-' + alignment_seq2
+				alignment_seq1 = seq1[i-1] + alignment_seq1
+				i -= 1
+			elif tb_val == 'top_new':
+				alignment_seq2 = '-' + alignment_seq2
+				alignment_seq1 = seq1[i-1] + alignment_seq1
+				i -= 1
+			elif tb_val == 'side_extended':
+				alignment_seq2 = seq2[j-1] + alignment_seq2
+				alignment_seq1 = '-' + alignment_seq1
+				j -= 1
+			elif tb_val == 'side_new':
+				alignment_seq2 = seq2[j-1] + alignment_seq2
+				alignment_seq1 = '-' + alignment_seq1
+				j -= 1
+			elif tb_val == 'diag':
+				alignment_seq2 = seq2[j-1] + alignment_seq2
+				alignment_seq1 = seq1[i-1] + alignment_seq1
+				i -= 1
+				j -= 1
+			elif tb_val == 'end':
 				break
-			if best_ind == 0:
-				i -= 1
-				alignment_seq1 += seq1[i]
-				alignment_seq2 += '-'
-			elif best_ind == 1:
-				j -= 1
-				alignment_seq1 += '-'
-				alignment_seq2 += seq2[j]
-			elif best_ind == 2:
-				i -= 1
-				j -= 1
-				alignment_seq1 += seq1[i]
-				alignment_seq2 += seq2[j]
 		self.alignment_ = [alignment_seq1, alignment_seq2]
 
 
 class NeedlemanWunsch(PairwiseAligner):
-	def __init__(self, substitution_matrix_file, gap_penalty):
-		super(NeedlemanWunsch, self).__init__(substitution_matrix_file)
-		self.gap_penalty = gap_penalty
+	def __init__(self, substitution_matrix_file, gap_open, gap_extend):
+		super(NeedlemanWunsch, self).__init__(substitution_matrix_file, gap_open, gap_extend)
 
 	def align(self, fasta1, fasta2):
 		'''
@@ -220,7 +264,7 @@ class NeedlemanWunsch(PairwiseAligner):
 		alignmnet. The alignment score is saved to the field
 		self.align_score_ and the alignments are saved as
 		a list of composition [seq1_alignment, seq2_alignment]
-		to the field self.aligment_.
+		to the field self.alignment_.
 
 		Args
 		----
@@ -231,46 +275,85 @@ class NeedlemanWunsch(PairwiseAligner):
 		-------
 		None
 		'''
-		# Initiating Sequences and Scoring Matirx
-		self._initiate_seqs(fasta_1, fasta_2)
-		self.score_matrix = self._initiate_score_matrix()
-		s_matrix = self.score_matrix
+		# Initializing Sequences
+		self._initiate_seqs(fasta1, fasta2)
+		seq1 = self.seq1
+		seq2 = self.seq2
+		# Initializing Core Score Matrix
+		self.score_matrix = self.initiate_matrix()
+		s_mat = self.score_matrix
+		# Initializing Additional Scoring Matrices for Indels (Gaps)
+		x_mat = self.initiate_matrix() # seq2 gap matrix
+		x_mat[:, 0] = np.arange(len(seq1)+1)*-self.gap_extend - self.gap_open
+		x_mat[0, :] = np.arange(len(seq2)+1)*-self.gap_open*100000000
+		y_mat = self.initiate_matrix() # seq1 gap matrix
+		y_mat[:, 0] = np.arange(len(seq1)+1)*-self.gap_open*100000000
+		y_mat[0, :] = np.arange(len(seq2)+1)*-self.gap_extend - self.gap_open
+		# Initialize Traceback matrix
+		tb_mat = self.initiate_matrix().astype('str')
+		# Filling in first column and row
+		s_mat[1:, 0] = np.arange(len(seq1)+1)[:-1]*-self.gap_open*100000000
+		s_mat[0, 1:] = np.arange(len(seq2)+1)[:-1]*-self.gap_open*100000000
+		# Initializing traceback matrix
+		tb_mat = self.initiate_matrix().astype('str')
 		# Filling in score Matrix
-		for i in range(s_matrix.shape[0]):
-			for j in range(s_matrix.shape[1]):
-				if i == j and i == 0:
-					pass
-				else:
-					top = s_matrix[i-1, j] - self.gap_penalty
-					side = s_matrix[i, j-1] - self.gap_penalty
-					diag = s_matrix[i-1, j-1] + self.sub_matrix[(seq1[i], seq2[j])]
-					s_matrix[i, j] = max([top, side, diag])
+		for i in range(s_mat.shape[0])[1:]:
+			for j in range(s_mat.shape[1])[1:]:
+				# t gap matrix filling
+				gap_dict = {1: 'new_gap', 0: 'extended_gap'}
+				new_gap = (s_mat[i-1, j] - self.gap_open)
+				extended_gap = (x_mat[i-1, j] - self.gap_extend)
+				x_mat[i, j] = max([extended_gap, new_gap])
+				gap_type_x = gap_dict[np.argmax([extended_gap, new_gap])]
+				# u gap matrix filling
+				new_gap = (s_mat[i, j-1] - self.gap_open)
+				extended_gap = (y_mat[i, j-1] - self.gap_extend)
+				y_mat[i, j] = max([extended_gap, new_gap])
+				gap_type_y = gap_dict[np.argmax([extended_gap, new_gap])]
+				# Taking maximum transition
+				top = x_mat[i, j]
+				side = y_mat[i, j]
+				diag = s_mat[i-1, j-1] + self.sub_matrix[(seq1[i-1], seq2[j-1])]
+				s_mat[i,j] = max((top, side, diag))
+				max_ind = np.argmax((top, side, diag))
+				# filling in traceback matrix
+				if max_ind == 0 and gap_type_x == 'new_gap':
+					tb_mat[i, j] = 'top_new'
+				elif max_ind == 0 and gap_type_x == 'extended_gap':
+					tb_mat[i, j] = 'top_extended'
+				elif max_ind == 1 and gap_type_y == 'new_gap':
+					tb_mat[i, j] = 'side_new'
+				elif max_ind == 1 and gap_type_y == 'extended_gap':
+					tb_mat[i, j] = 'side_extended'
+				elif max_ind == 2:
+					tb_mat[i, j] = 'diag'
 		# Traceback
-		alignment_seq1 = seq1[-1]
-		alignment_seq2 = seq2[-1]
-		###### NEED TO MODIFY THIS ######
-		# max_ind = np.unravel_index(np.argmax(s_matrix, axis=None), s_matrix.shape)
-		# self.align_score_ = np.amax(s_matrix)
-		# i, j = max_ind
-		# alignment_seq1 = seq1[i]
-		# alignment_seq2 = seq2[j]
+		alignment_seq1 = ''
+		alignment_seq2 = ''
+		i = len(seq1)
+		j = len(seq2)
 		while i > 0 and j > 0:
-			top = s[i-1, j]
-			side = s[i, j-1]
-			diag = s[i-1, j-1]
-			best_ind = np.argmax(np.array([top, side, diag]))
-			best_max = np.amax(np.array([top, side, diag]))
-			if best_ind == 0:
+			tb_val = tb_mat[i, j]
+			if tb_val == 'top_extended':
+				alignment_seq2 = '-' + alignment_seq2
+				alignment_seq1 = seq1[i-1] + alignment_seq1
 				i -= 1
-				alignment_seq1 += seq1[i]
-				alignment_seq2 += '-'
-			elif best_ind == 1:
+			elif tb_val == 'top_new':
+				alignment_seq2 = '-' + alignment_seq2
+				alignment_seq1 = seq1[i-1] + alignment_seq1
+				i -= 1
+			elif tb_val == 'side_extended':
+				alignment_seq2 = seq2[j-1] + alignment_seq2
+				alignment_seq1 = '-' + alignment_seq1
 				j -= 1
-				alignment_seq1 += '-'
-				alignment_seq2 += seq2[j]
-			elif best_ind == 2:
+			elif tb_val == 'side_new':
+				alignment_seq2 = seq2[j-1] + alignment_seq2
+				alignment_seq1 = '-' + alignment_seq1
+				j -= 1
+			elif tb_val == 'diag':
+				alignment_seq2 = seq2[j-1] + alignment_seq2
+				alignment_seq1 = seq1[i-1] + alignment_seq1
 				i -= 1
 				j -= 1
-				alignment_seq1 += seq1[i]
-				alignment_seq2 += seq2[j]
 		self.alignment_ = [alignment_seq1, alignment_seq2]
+		self.align_score_ = s_mat[-1, -1]
--- test/test_algs.py
+++ test/test_algs.py
@@ -6,86 +6,100 @@ def some_relevant_data():
 	return np.ones(10)
 
 def test_fasta_io_sw():
-	sw_aligner = algs.SmithWaterman('../scoring_matrices/BLOSUM50.mat', [4, 1])
-	seq_t1, header_t1 = sw_aligner._read_fasta('test1.fa')
-	seq_t2, header_t2 = sw_aligner._read_fasta('test2.fa')
+	sw_aligner = algs.SmithWaterman('./scoring_matrices/BLOSUM50.mat', 10, 0.5)
+	seq_t1, header_t1 = sw_aligner._read_fasta('./test/test1.fa')
+	seq_t2, header_t2 = sw_aligner._read_fasta('./test/test2.fa')
 	assert seq_t1 == 'SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM'
 	assert seq_t2 == 'GAWDDAMDAWG'
 	assert header_t1 == '>test_1_fasta'
 	assert header_t2 == '>test_2_fasta'
 
 def test_fasta_io_nw():
-	nw_aligner = algs.NeedlemanWunsch('../scoring_matrices/BLOSUM50.mat', 1)
-	seq_t1, header_t1 = nw_aligner._read_fasta('test1.fa')
-	seq_t2, header_t2 = nw_aligner._read_fasta('test2.fa')
+	nw_aligner = algs.NeedlemanWunsch('./scoring_matrices/BLOSUM50.mat', 10, 0.5)
+	seq_t1, header_t1 = nw_aligner._read_fasta('./test/test1.fa')
+	seq_t2, header_t2 = nw_aligner._read_fasta('./test/test2.fa')
 	assert seq_t1 == 'SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM'
 	assert seq_t2 == 'GAWDDAMDAWG'
 	assert header_t1 == '>test_1_fasta'
 	assert header_t2 == '>test_2_fasta'
 
 def test_seq_initiation_sw():
-	sw_aligner = algs.SmithWaterman('../scoring_matrices/BLOSUM50.mat', [4, 1])
-	sw_aligner._initiate_seqs('test1.fa', 'test2.fa')
+	sw_aligner = algs.SmithWaterman('./scoring_matrices/BLOSUM50.mat', 10, 0.5)
+	sw_aligner._initiate_seqs('./test/test1.fa', './test/test2.fa')
 	assert sw_aligner.seq1 == 'SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM'
 	assert sw_aligner.seq2 == 'GAWDDAMDAWG'
 	assert sw_aligner.header1 == '>test_1_fasta'
 	assert sw_aligner.header2 == '>test_2_fasta'
 
 def test_seq_initiation_nw():
-	nw_aligner = algs.NeedlemanWunsch('../scoring_matrices/BLOSUM50.mat', 1)
-	nw_aligner._initiate_seqs('test1.fa', 'test2.fa')
+	nw_aligner = algs.NeedlemanWunsch('./scoring_matrices/BLOSUM50.mat', 10, 0.5)
+	nw_aligner._initiate_seqs('./test/test1.fa', './test/test2.fa')
 	assert nw_aligner.seq1 == 'SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM'
 	assert nw_aligner.seq2 == 'GAWDDAMDAWG'
 	assert nw_aligner.header1 == '>test_1_fasta'
 	assert nw_aligner.header2 == '>test_2_fasta'
 
 def test_score_matrix_gen_sw():
-	sw_aligner = algs.SmithWaterman('../scoring_matrices/BLOSUM50.mat', [4, 1])
-	sw_aligner._initiate_seqs('test1.fa', 'test2.fa')
-	sw_aligner.score_matrix = self._initiate_score_matrix()
-	seq1 = nw_aligner.seq1
-	seq2 = nw_alginer.seq2
-	assert sw_aligner.score_matrix.shape == (len(seq1), len(seq2))
+	sw_aligner = algs.SmithWaterman('./scoring_matrices/BLOSUM50.mat', 10, 0.5)
+	sw_aligner._initiate_seqs('./test/test1.fa', './test/test2.fa')
+	sw_aligner.score_matrix = sw_aligner.initiate_matrix()
+	seq1 = sw_aligner.seq1
+	seq2 = sw_aligner.seq2
+	assert sw_aligner.score_matrix.shape == (len(seq1)+1, len(seq2)+1)
 
 def test_score_matrix_gen_nw():
-	nw_aligner = algs.NeedlemanWunsch('../scoring_matrices/BLOSUM50.mat', 1)
-	nw_aligner._initiate_seqs('test1.fa', 'test2.fa')
-	nw_aligner.score_matrix = self._initiate_score_matrix()
+	nw_aligner = algs.NeedlemanWunsch('./scoring_matrices/BLOSUM50.mat', 10, 0.5)
+	nw_aligner._initiate_seqs('./test/test1.fa', './test/test2.fa')
+	nw_aligner.score_matrix = nw_aligner.initiate_matrix()
 	seq1 = nw_aligner.seq1
-	seq2 = nw_alginer.seq2
-	assert nw_aligner.score_matrix.shape == (len(seq1), len(seq2))
+	seq2 = nw_aligner.seq2
+	assert nw_aligner.score_matrix.shape == (len(seq1)+1, len(seq2)+1)
 
 def test_scoring_matrix_io_sw():
-	assert True
+	sw_aligner = algs.SmithWaterman('./scoring_matrices/BLOSUM62.mat', 10, 0.5)
+
 
 def test_scoring_matrix_io_nw():
 	assert True
 
 def test_identical_sw():
-	sw_aligner = algs.SmithWaterman('../scoring_matrices/BLOSUM62.mat', [10, 0.5])
-	sw_aligner.align('test3.fa', 'test3.fa')
+	sw_aligner = algs.SmithWaterman('./scoring_matrices/BLOSUM62.mat', 10, 0.5)
+	sw_aligner.align('./test/test3.fa', './test/test3.fa')
 	assert sw_aligner.align_score_ == 733.0
-	assert sw_aligner.alignment_ == ['SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM',
-	'SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM']
-	assert True
+	assert sw_aligner.alignment_ == ['MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHGKKVADALTNAVAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPAEFTPAVHASLDKFLASVSTVLTSKYR',
+	'MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHGKKVADALTNAVAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPAEFTPAVHASLDKFLASVSTVLTSKYR']
 
 def test_identical_nw():
-	nw_alginer = algs.NeedlemanWunsch('../scoring_matrices/BLOSUM62.mat', 1)
-	nw_alginer.align('test3.fa', 'test3.fa')
-	assert nw_aligner.align_score_ == 734.0
-	assert nw_aligner.alignment_ == ['SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM',
-	'SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM']
+	nw_aligner = algs.NeedlemanWunsch('./scoring_matrices/BLOSUM62.mat', 10, 0.5)
+	nw_aligner.align('./test/test3.fa', './test/test3.fa')
+	assert nw_aligner.align_score_ == 733.0
+	assert nw_aligner.alignment_ == ['MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHGKKVADALTNAVAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPAEFTPAVHASLDKFLASVSTVLTSKYR',
+	'MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHGKKVADALTNAVAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPAEFTPAVHASLDKFLASVSTVLTSKYR']
 
 def test_alignment_score_sw():
-	sw_aligner = algs.SmithWaterman('../scoring_matrices/BLOSUM62.mat', [10, 0.5])
-	sw_aligner.align('test3.fa', 'test4.fa')
+	sw_aligner = algs.SmithWaterman('./scoring_matrices/BLOSUM62.mat', 10, 0.5)
+	sw_aligner.align('./test/test3.fa', './test/test4.fa')
 	assert sw_aligner.align_score_ == 648.0
-	assert sw_aligner.alignment_ == ['SLEAAQKSNVTSSWAKASAAWGTAGPEFFMALFDAHDDVFAKFSGLFSGAAKGTVKNTPEMAAQAQSFKGLVSNWVDNLDNAGALEGQCKTFAANHKARGISAGQLEAAFKVLSGFMKSYGGDEGAWTAVAGALMGEIEPDM',
+	assert sw_aligner.alignment_ == ['MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHGKKVADALTNAVAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPAEFTPAVHASLDKFLASVSTVLTSKYR',
 	'MVLSGEDKSNIKAAWGKIGGHGAEYGAEALERMFASFPTTKTYFPHFDVSHGSAQVKGHGKKVADALASAAGHLDDLPGALSALSDLHAHKLRVDPVNFKLLSHCLLVTLASHHPADFTPAVHASLDKFLASVSTVLTSKYR']
 
 def test_alignment_score_nw():
-	nw_aligner = algs.NeedlemanWunsch('../scoring_matrices/BLOSUM62.mat', 1)
-	nw_aligner.align('test3.fa', 'test4.fa')
-	assert sw_aligner.align_score_ == 656.0
-	assert sw_aligner.alignment_ == ['MVLSPA-DKTNVKAAWGKVGAHAG-EYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHGKKVADALTNAVA-HVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHL-PAEFTPAVHASLDKFLASVSTVLTSKYR',
-	'MVLS-GEDKSNIKAAWGKIGGH-GAEYGAEALERMFASFPTTKTYFPHFDVSHGSAQVKGHGKKVADALASA-AGHLDDLPGALSALSDLHAHKLRVDPVNFKLLSHCLLVTLASH-HPADFTPAVHASLDKFLASVSTVLTSKYR']
+	nw_aligner = algs.NeedlemanWunsch('./scoring_matrices/BLOSUM62.mat', 10, 0.5)
+	nw_aligner.align('./test/test3.fa', './test/test4.fa')
+	assert nw_aligner.align_score_ == 648.0
+	assert nw_aligner.alignment_ == ['MVLSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHFDLSHGSAQVKGHGKKVADALTNAVAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPAEFTPAVHASLDKFLASVSTVLTSKYR',
+	'MVLSGEDKSNIKAAWGKIGGHGAEYGAEALERMFASFPTTKTYFPHFDVSHGSAQVKGHGKKVADALASAAGHLDDLPGALSALSDLHAHKLRVDPVNFKLLSHCLLVTLASHHPADFTPAVHASLDKFLASVSTVLTSKYR']
+
+def test_alignment_score_sw_2():
+	sw_aligner = algs.SmithWaterman('./scoring_matrices/BLOSUM62.mat', 10, 0.5)
+	sw_aligner.align('./test/test5.fa', './test/test6.fa')
+	assert sw_aligner.align_score_ == 591.5
+	assert sw_aligner.alignment_ == ['TSPASIRPPAGPSSRPAMVSSRRTRPSPPGPRRPTGRPCCSAAPRRPQATGGWKTCSGTCTTSTSTRHRGRSGWSARTTTAACLRASRKSMRAACSRSAGSRPNRFAPTLMSSCITSTTGPPAWAGDRSHE',
+	'TSPASIRPPAGPSSR---------RPSPPGPRRPTGRPCCSAAPRRPQATGGWKTCSGTCTTSTSTRHRGRSGW----------RASRKSMRAACSRSAGSRPNRFAPTLMSSCITSTTGPPAWAGDRSHE']
+
+def test_alignment_score_nw_2():
+	nw_aligner = algs.NeedlemanWunsch('./scoring_matrices/BLOSUM62.mat', 10, 0.5)
+	nw_aligner.align('./test/test5.fa', './test/test6.fa')
+	assert nw_aligner.align_score_ == 591.5
+	assert nw_aligner.alignment_ == ['TSPASIRPPAGPSSRPAMVSSRRTRPSPPGPRRPTGRPCCSAAPRRPQATGGWKTCSGTCTTSTSTRHRGRSGWSARTTTAACLRASRKSMRAACSRSAGSRPNRFAPTLMSSCITSTTGPPAWAGDRSHE',
+	'TSPASIRPPAGPSSR---------RPSPPGPRRPTGRPCCSAAPRRPQATGGWKTCSGTCTTSTSTRHRGRSGW----------RASRKSMRAACSRSAGSRPNRFAPTLMSSCITSTTGPPAWAGDRSHE']
